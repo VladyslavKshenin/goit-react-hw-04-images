bec2a08606e5a192aa9517795bd961022bdaee34
VladyslavKshenin <legendary.1bk@gmail.com>
2024-01-18 16:54:37 +0200

    refactoring app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,126 +1,96 @@
-import { Component } from 'react';
+import { useEffect, useState} from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import css from './App.module.css';
 import Searchbar from './Searchbar/Searchbar';
 import Loader from './Loader/Loader';
 import ImageGallery from './ImageGalery/ImageGallery';
 import Button from './Button/Button';
 import api from './Api/api';
 
-export default class App extends Component {
-  state = {
-    searchQuery: '',
-    pendingRequest: false,
-    page: 1,
-    picturesSet: [],
-    searchMatches: 0,
-    totalHits: 0,
-  };
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [pendingRequest, setPendingRequest] = useState(false);
+  const [page, setPage] = useState(1);
+  const [picturesSet, setPicturesSet] = useState([]);
+  const [searchMatches, setSearchMatches] = useState(0);
+  const [totalHits, setTotalHits] = useState(0);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { searchQuery, page } = this.state;
-    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
-      this.setState({ pendingRequest: true });
-      api(searchQuery, page)
-        .then(pictures => {
-          this.setState(
-            prevState => ({
-              pendingRequest: false,
-              picturesSet: [...prevState.picturesSet, ...pictures.hits],
-              searchMatches: prevState.searchMatches + pictures.hits.length,
-              totalHits: pictures.totalHits,
-            }),
-            () => {
-              // console.log(
-              //   `we have loaded ${this.state.searchMatches} out of ${this.state.totalHits} totally found`
-              // );
-              this.handleSearchNotifications();
-            }
-          );
-        })
-        .catch(error => this.handleError(error));
-    }
-  }
+  const fetchData = async () => {
+    try {
+      if (!searchQuery) {
+        return;
+      }
 
-  handleSearchNotifications = () => {
-    const { page, searchMatches, totalHits } = this.state;
-    if (totalHits > 0 && page === 1) {
-      toast.success(`Hooray! We found ${totalHits} images`, {
-        position: 'top-right',
-        theme: 'colored',
-      });
-    } else if (searchMatches === totalHits && totalHits > 0) {
-      toast.info("You've reached the end of search results.", {
-        position: 'top-right',
-        theme: 'colored',
-      });
-    } else if (totalHits === 0) {
-      toast.warn(
-        'No pictures were found for your query, please try another one!',
-        {
+      setPendingRequest(true);
+
+      const pictures = await api(searchQuery, page);
+
+      setPendingRequest(false);
+      setPicturesSet((prevPicturesSet) => [...prevPicturesSet, ...pictures.hits]);
+      setTotalHits(pictures.totalHits);
+      setSearchMatches((prevSearchMatches) => prevSearchMatches + pictures.hits.length);
+
+      if (pictures.totalHits > 0 && page === 1) {
+        toast.success(`Hooray! We found ${pictures.totalHits} images`, {
           position: 'top-right',
           theme: 'colored',
-        }
-      );
+        });
+      } else if (searchMatches === pictures.totalHits && pictures.totalHits > 0) {
+        toast.info("You've reached the end of search results.", {
+          position: 'top-right',
+          theme: 'colored',
+        });
+      } else if (pictures.totalHits === 0) {
+        toast.warn('No pictures were found for your query, please try another one!', {
+          position: 'top-right',
+          theme: 'colored',
+        });
+      }
+    } catch (error) {
+      handleError(error);
     }
   };
 
-  handleError = error => {
-    this.setState({ pendingRequest: false });
-    console.log('An error occurred: ', error.message);
+  useEffect(() => {
+    fetchData();
+  }, [searchQuery, page]);
+
+  const handleError = (error) => {
+    setPendingRequest(false);
     toast.error(`An error occurred: ${error.message}`, {
       position: 'top-right',
       theme: 'colored',
     });
   };
 
-  handleQuery = query => {
-    const { searchQuery } = this.state;
+  const handleQuery = (query) => {
     if (query === searchQuery) {
-      toast.warn(
-        `You are searching through the collection for "${query}" already!`,
-        {
-          position: 'top-right',
-          theme: 'colored',
-        }
-      );
-    }
-    else {
-      this.setState({
-        searchQuery: query,
-        page: 1,
-        picturesSet: [],
-        searchMatches: 0,
-        totalHits: 0,
+      toast.warn(`You are searching through the collection for "${query}" already!`, {
+        position: 'top-right',
+        theme: 'colored',
       });
+    } else {
+      setSearchQuery(query);
+      setPage(1);
+      setPicturesSet([]);
+      setSearchMatches(0);
+      setTotalHits(0);
     }
   };
 
-  loadMorePictures = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const loadMorePictures = () => {
+    setPage((prevPage) => prevPage + 1);
   };
 
-  render() {
-    const { pendingRequest, picturesSet, searchMatches, totalHits } =
-      this.state;
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleQuery} />
-        {picturesSet.length > 0 && <ImageGallery pictures={picturesSet} />}
-        {pendingRequest && <Loader />}
-        {searchMatches < totalHits && (
-          <Button onClick={this.loadMorePictures} />
-        )}
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={handleQuery} />
+      {picturesSet.length > 0 && <ImageGallery pictures={picturesSet} />}
+      {pendingRequest && <Loader />}
+      {searchMatches < totalHits && <Button onClick={loadMorePictures} />}
 
-        <ToastContainer
-          autoClose={3000}
-          hideProgressBar={false}
-          closeOnClick
-          pauseOnHover
-        />
-      </div>
-    );
-  }
+      <ToastContainer autoClose={3000} hideProgressBar={false} closeOnClick pauseOnHover />
+    </div>
+  );
 }
